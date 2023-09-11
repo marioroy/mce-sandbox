bc9f9387d47aa2f506eddcbb195ce268b49e546f
Mario Roy <marioeroy@gmail.com>
2023-09-11 05:04:58 -0500

    Add build scripts

---
+++ demos/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Ensure 'gcc' and 'codon' executables are in your path.
+# Specify the full path otherwise.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CODON="codon"
+CC="gcc"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Edit this script to your likely. It is ignored by git.
+# Uncomment the items you wish to build.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ${CODON} build -release -o primes2 primes2.codon
+# ${CODON} build -release -o primes4 primes4.codon
+
+  ${CC} -o primes1 -O3 -fopenmp -I../src primes1.c -lm
+  ${CC} -o primes3 -O3 -fopenmp primes3.c -lprimesieve -lm
+
---
+++ examples/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Ensure 'gcc', 'codon', and 'nvcc' executables are in your path.
+# Specify the full path otherwise.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+NVCC="nvcc"
+CODON="codon"
+CC="gcc"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Edit this script to your likely. It is ignored by git.
+# Uncomment the items you wish to build.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ${NVCC} -o cudasieve -I../src -O3 -prec-sqrt=true cudasieve.cu -lm
+
+# ${CODON} build -release -o cpusieve cpusieve.codon
+# ${CODON} build -release -o gpusieve gpusieve.codon
+# ${CODON} build -release -o pgpusieve pgpusieve.codon
+
+  ${CC} -o practicalsieve -I../src -O3 practicalsieve.c -lm
+  ${CC} -o prangesieve -I../src -O3 -fopenmp prangesieve.c -lm
+  ${CC} -o rangesieve -I../src -O3 rangesieve.c -lm
+  ${CC} -o segmentsieve -I../src -O3 segmentsieve.c -lm
+
