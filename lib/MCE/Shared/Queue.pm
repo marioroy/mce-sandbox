6407590b5665a8949c042f9e9568319dde57c5fa
Mario Roy <marioeroy@gmail.com>
2024-06-10 04:21:21 -0500

    Bump MCE and MCE::Shared
    1. MCE 1.895
    2. MCE::Shared 1.890

--- lib/MCE/Shared/Queue.pm
+++ lib/MCE/Shared/Queue.pm
@@ -13,7 +13,7 @@ use 5.010001;
 
 no warnings qw( threads recursion uninitialized numeric );
 
-our $VERSION = '1.889';
+our $VERSION = '1.890';
 
 ## no critic (Subroutines::ProhibitExplicitReturnUndef)
 
@@ -1025,7 +1025,7 @@ MCE::Shared::Queue - Hybrid-queue helper class
 
 =head1 VERSION
 
-This document describes MCE::Shared::Queue version 1.889
+This document describes MCE::Shared::Queue version 1.890
 
 =head1 DESCRIPTION
 
